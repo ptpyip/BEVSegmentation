7863fb71c0c5d92605532817357c669050689a19
ptpyip <ptpyip@connect.ust.hk>
2023-04-20 17:19:55 +0800

    fix import errors

--- mmsegBEV/models/builder.py
+++ mmsegBEV/models/builder.py
@@ -19,14 +19,6 @@ def build_neck(cfg):
     return NECKS.build(cfg)
 
 
-def build_vtransform(cfg):
-    return VTRANSFORMS.build(cfg)
-
-
-def build_fuser(cfg):
-    return FUSERS.build(cfg)
-
-
 def build_head(cfg):
     return HEADS.build(cfg)
 
@@ -35,11 +27,11 @@ def build_loss(cfg):
     return LOSSES.build(cfg)
 
 
-def build_BEVFormer(cfg, train_cfg=None, test_cfg=None):
-    return BEVFormer.build(
+def build_segmentor(cfg, train_cfg=None, test_cfg=None):
+    return SEGMENTORS.build(
         cfg, default_args=dict(train_cfg=train_cfg, test_cfg=test_cfg)
     )
 
 
 def build_model(cfg, train_cfg=None, test_cfg=None):
-    return build_BEVFormer(cfg, train_cfg=train_cfg, test_cfg=test_cfg)
+    return build_segmentor(cfg, train_cfg=train_cfg, test_cfg=test_cfg)
