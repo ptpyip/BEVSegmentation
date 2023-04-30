61ead84835bad79d78b00300af82d76858983e30
ptpyip <ptpyip@connect.ust.hk>
2023-04-30 10:54:44 +0800

    update evaluation hook

--- mmsegBEV/evaluation/__init__.py
+++ mmsegBEV/evaluation/__init__.py
@@ -1,2 +1,4 @@
 from .eval_hooks import DistEvalHook
+# from mmcv.runner import EvalHook, DistEvalHook      # no long need custom eval hook
+from mmcv.runner import EvalHook      # no long need custom eval hook
 from .metrics import *
